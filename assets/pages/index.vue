500fdff6dce0557664ae3a683babcb50d58c4d39
aCuriousDev <quentinmarius@gmail.com>
2023-11-14 13:42:45 +0100

    feat(index.vue): Add Response Selection and Validation
    Implemented the ability to select individual responses for each depot in the 'index.vue' component.
    Added checkboxes for each response and a 'Validate Responses' button.
    The button is enabled only when one or more responses are selected.
    It triggers the validation logic (currently a placeholder) and resets the selection state.

--- assets/pages/index.vue
+++ assets/pages/index.vue
@@ -5,44 +5,130 @@
       <p class="text-xl">Listing des demandes cliniques</p>
     </div>
     <div class="flex gap-2 flex-col w-full">
-      <div 
+      <div
         v-for="depot in depots"
         :key="depot.id"
         class="bg-white rounded-xl shadow-sm p-4"
       >
-        <p class="text-base font-semibold">Titre: <span class="text-base text-gray-700 font-light">{{ depot.titre }}</span></p>
-        <p class="text-base font-semibold">Description: <span class="text-base text-gray-700 font-light">{{ depot.description }}</span></p>
-        <p class="text-base font-semibold">Date de création: <span class="text-base text-gray-700 font-light">{{ depot.date_creation }}</span></p>
-        <div class="my-4 p-2 border border-gray rounded-xl bg-gray-100 flex flex-col gap-2" v-if="depot.reponses.length">
-          <div class="border border-dashed border-2 bg-white px-4 py-2" v-for="reponse in depot.reponses" :key="reponse.id">
-            <p class="text-base font-semibold text-red-500">Type: <span class="text-base text-gray-700 font-light">{{ getTypeLabel(reponse.type) }}</span></p>
-            <p class="text-base font-semibold">Titre: <span class="text-base text-gray-700 font-light">{{ reponse.titre }}</span></p>
-            <p class="text-base font-semibold">Description: <span class="text-base text-gray-700 font-light">{{ reponse.description }}</span></p>
-            <p class="text-base font-semibold">Date de création: <span class="text-base text-gray-700 font-light">{{ reponse.date_creation }}</span></p>
+        <p class="text-base font-semibold">
+          Titre: <span class="text-base text-gray-700 font-light">{{ depot.titre }}</span>
+        </p>
+        <p class="text-base font-semibold">
+          Description:
+          <span class="text-base text-gray-700 font-light">{{ depot.description }}</span>
+        </p>
+        <p class="text-base font-semibold">
+          Date de création:
+          <span class="text-base text-gray-700 font-light">{{
+            depot.date_creation
+          }}</span>
+        </p>
+
+        <div
+          class="my-4 p-2 border border-gray rounded-xl bg-gray-100 flex flex-col gap-2"
+          v-if="depot.reponses.length"
+        >
+          <div
+            v-for="reponse in depot.reponses"
+            :key="reponse.id"
+            class="border border-dashed border-2 bg-white px-4 py-2 flex items-center gap-2"
+          >
+            <input
+              type="checkbox"
+              v-model="selectedResponses[depot.id]"
+              :value="reponse.id"
+            />
+            <div>
+              <p class="text-base font-semibold text-red-500">
+                Type:
+                <span class="text-base text-gray-700 font-light">{{
+                  getTypeLabel(reponse.type)
+                }}</span>
+              </p>
+              <p class="text-base font-semibold">
+                Titre:
+                <span class="text-base text-gray-700 font-light">{{
+                  reponse.titre
+                }}</span>
+              </p>
+              <p class="text-base font-semibold">
+                Description:
+                <span class="text-base text-gray-700 font-light">{{
+                  reponse.description
+                }}</span>
+              </p>
+              <p class="text-base font-semibold">
+                Date de création:
+                <span class="text-base text-gray-700 font-light">{{
+                  reponse.date_creation
+                }}</span>
+              </p>
+            </div>
           </div>
         </div>
+
         <div class="flex items-center justify-center" v-else>
           <p class="text-base font-semibold">Aucune réponse</p>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2" @click="$router.push(`/depots/${depot.id}`)">Répondre à la demande</button>
+
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          @click="$router.push(`/depots/${depot.id}`)"
+        >
+          Répondre à la demande
+        </button>
+
+        <button
+          class="mt-2 py-2 px-4 rounded font-bold"
+          :class="
+            selectedResponses[depot.id] && selectedResponses[depot.id].length
+              ? 'bg-green-500 hover:bg-green-700 text-white'
+              : 'bg-gray-500 text-white'
+          "
+          @click="validateResponses(depot.id)"
+          v-if="depot.reponses.length"
+        >
+          Valider les réponses
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { getLabel } from '@/enum/demande_clinique/reponse/type';
+import { mapGetters } from "vuex";
+import { getLabel } from "@/enum/demande_clinique/reponse/type";
 
 export default {
-  name: 'Index',
+  name: "Index",
+  data() {
+    return {
+      selectedResponses: {},
+    };
+  },
   computed: {
     ...mapGetters({
-      depots: 'demande_clinique/depots',
+      depots: "demande_clinique/depots",
     }),
+  },
+  created() {
+    // Initialize the selected responses with empty arrays for each depot
+    this.depots.forEach((depot) => {
+      this.$set(this.selectedResponses, depot.id, []);
+    });
   },
   methods: {
     getTypeLabel: getLabel,
-  }
+    validateResponses(depotId) {
+      const selected = this.selectedResponses[depotId];
+      if (selected && selected.length) {
+        // Add validation logic here
+        // Reset the selected responses after validation
+        this.selectedResponses[depotId] = [];
+      } else {
+        alert("No responses selected for validation");
+      }
+    },
+  },
 };
 </script>
